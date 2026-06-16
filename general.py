7d2a4712aa275b111ee77585c1b4914628f24777
Félix <97846369+dalekpascontent@users.noreply.github.com>
2026-06-16 14:14:13 +0200

    Update general.py

--- general.py
+++ general.py
@@ -1,7 +1,7 @@
 
 import numpy as np 
 import matplotlib.pyplot as plt 
-
+from scipy import optimize
 
 #Parametres de modelisation 
 
@@ -161,6 +161,37 @@ def show_E_k(V,B):
     plt.xlabel("Wavenumber")
     plt.ylabel("Spectral Energy (J.m/s)")
     plt.show()
+
+
+def segments_fit(X, Y, count):
+    xmin = X.min()
+    xmax = X.max()
+
+    seg = np.full(count - 1, (xmax - xmin) / count)
+
+    px_init = np.r_[np.r_[xmin, seg].cumsum(), xmax]
+    py_init = np.array([Y[np.abs(X - x) < (xmax - xmin) * 0.01].mean() for x in px_init])
+
+    def func(p):
+        seg = p[:count - 1]
+        py = p[count - 1:]
+        px = np.r_[np.r_[xmin, seg].cumsum(), xmax]
+        return px, py
+
+    def err(p):
+        px, py = func(p)
+        Y2 = np.interp(X, px, py)
+        return np.mean((Y - Y2)**2)
+
+    r = optimize.minimize(err, x0=np.r_[seg, py_init], method='Nelder-Mead')
+    return func(r.x)
+
+
+def plot_multi_fit (X,Y,count):
+    px, py = segments_fit(X, Y, count)
+
+    plt.plot(X, Y, ".")
+    plt.plot(px, py, "-or");
 
 show_inv(U, B)
 show_E_k(U, B)
