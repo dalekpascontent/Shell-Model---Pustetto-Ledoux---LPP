3a7b9ff0ddd66dbf63d0602c1e5e0d2da0a4d3b6
Azuria-p <axel.pustetto@gmail.com>
2026-06-17 11:44:44 +0200

    Update general.py

--- general.py
+++ general.py
@@ -5,47 +5,30 @@ from scipy import optimize
 
 
 
-#Parametres de modelisation 
-
-N = 23 #Exposant du k_max
-
-P = int(1e5) #Indice de discretisation du temps
-T = 100 #Temps total de modélisation
-h = T/P
-k0 = 0.04
-di = 0
-k = np.array([k0*(2**i) for i in range(N)])
-
-
-def CI():
+def CI(parametre, Umoy, Bmoy):
+    N, P, k0, k, dt, di, nu, eta = parametre
     V = np.zeros(N, dtype=complex)
     B = np.zeros(N, dtype=complex)
     sigma_k = k0  # largeur de la gaussienne en k, centrée sur k0
     for i in range(N):
-        enveloppe = np.exp(-(k[i] - k0)**2 / (4 * sigma_k**2))  # gaussienne en k
+        enveloppe = np.exp(-(k[i] - 0.04)**2 / (4 * sigma_k**2))  # gaussienne en k
         phase = np.exp(1j * 2 * np.pi * np.random.rand())    # phase aléatoire
-        V[i] = enveloppe * phase   # pic à k0
-        B[i] = enveloppe * phase   
+        V[i] = Umoy*enveloppe * phase   # pic à k0
+        B[i] = Bmoy*enveloppe * phase   
     return V, B
 
 
 ############## Partie intégration ###############
 
-v0, b0 = CI()
-dt = T/P
-nu = 1e-13
-eta = 1e-13
-
-
-U = np.zeros((P, N), dtype=complex)
-B = np.zeros((P, N), dtype=complex)
 
 
 # /!\ ATTENTION, les p qui vont suivre ne font pas reférence a la coquille n mais au p'ieme temps d'integration et le n correspond a la n'ieme coquille
 # Chaque couple dans V et B contient 2 listes, qui correspondent a toutes les couches au temps tp
 
 
-def NL(V, B):
+def NL(V, B, parametre):
+    N, P, k0, k, dt, di, nu, eta = parametre
+
     V_grand = np.zeros(N+4, dtype = complex)
     B_grand = np.zeros(N+4, dtype = complex)
     V_grand[2: N+2] = V  # On a donc V_grand = [0, 0, V, 0, 0]
@@ -71,17 +54,16 @@ def NL(V, B):
     return NLV, NLB
 
 
-expV = np.exp(-nu*(k**4)*dt) #lissage pour V 
-expB = np.exp(-eta*(k**4)*dt)
-
 #initialisation (méthode d'Euler pour calculer le terme au temps t1)
-def integ(v0, b0):
+def integ(v0, b0, parametre, expV, expB):
+    N, P, k0, k, dt, di, nu, eta = parametre
+
     U = np.zeros((P, N), dtype=complex)
     B = np.zeros((P, N), dtype=complex)
     U[0] = v0
     B[0] = b0
 
-    NLV, NLB = NL(v0, b0)
+    NLV, NLB = NL(v0, b0, parametre)
 
     Vmain = v0*expV + dt*NLV*expV #euler
     Bmain = b0*expB + dt*NLB*expB
@@ -90,7 +72,7 @@ def integ(v0, b0):
     fapV, fapB = NLV, NLB
 
     for i in range(2, P):
-        NLV, NLB = NL(Vmain, Bmain)
+        NLV, NLB = NL(Vmain, Bmain, parametre)
         V_2 = Vmain*expV + dt*(3/2*NLV*expV - 1/2*fapV*expV) 
         B_2 = Bmain*expB + dt*(3/2*NLB*expB - 1/2*fapB*expB)
         fapV, fapB = NLV, NLB  #(f(tp, ap), on l'avait calculé, on le sauvegarde pour le prochain calcul
@@ -100,13 +82,13 @@ def integ(v0, b0):
         B[i] = Bmain
     return U, B
 
-a = integ(v0, b0)
-U, B = a
 
 ################# Partie invariant ################
 
 
-def invariant(V, B):
+def invariant(V, B, parametre):
+    N, P, k0, k, dt, di, nu, eta = parametre
+
     l_E  = np.zeros(P)
     l_Hm = np.zeros(P)
     l_Hh = np.zeros(P)
@@ -120,19 +102,21 @@ def invariant(V, B):
             l_Hm[i] += ((-1)**(j+1)) * mod2_B / (2 * k[j])
             l_Hh[i] += ((-1)**(j+1) * di**2 * k[j] * mod2_V + di * 2 * croise) / 2
             
-    d_E = np.gradient(l_E,h)
-    d_Hm = np.gradient(l_Hm,h)
-    d_Hh = np.gradient(l_Hh,h)
+    d_E = np.gradient(l_E,dt)
+    d_Hm = np.gradient(l_Hm,dt)
+    d_Hh = np.gradient(l_Hh,dt)
     
     moy_dE = np.mean(d_E)
     moy_dHm = np.mean(d_Hm)
     moy_dHh = np.mean(d_Hh)
     
-    l_T = [ h*i for i in range(P)]
+    l_T = [ dt*i for i in range(P)]
     return [l_E,l_Hm,l_Hh,d_E,d_Hm,d_Hh,moy_dE,moy_dHm,moy_dHh,l_T]
 
 
-def E_kn(V, B):
+def E_kn(V, B, parametre):
+    N, P, k0, k, dt, di, nu, eta = parametre
+
     Ekn = np.zeros((P, N))
     for i in range(P):
         for j in range(N):
@@ -142,35 +126,38 @@ def E_kn(V, B):
             Ekn[i][j] = temp
     return Ekn
 
-def show_inv (V : list, B : list ):
+def show_inv (V : list, B : list, parametre ):
     
-    l_E, l_Hm, l_Hh, d_E, d_Hm, d_Hh, moy_dE, moy_dHm, moy_dHh, l_T = invariant(V, B)
+    l_E, l_Hm, l_Hh, d_E, d_Hm, d_Hh, moy_dE, moy_dHm, moy_dHh, l_T = invariant(V, B, parametre)
     
     print("Moyenne de la derivée de E :", moy_dE,"J/s")
     print("Moyenne de la derivée de Hm :", moy_dHm,"J.m/s")
     print("Moyenne de la derivée de Hh :", moy_dHh,"J.m/s")
     
     fig, axs = plt.subplots(3, sharex=True)
-    axs[0].plot(l_T,d_E, label = " Dérivée de E (J/s) ")
+    axs[0].plot(l_T,d_E, 'x', label = " Dérivée de E (J/s) ")
     axs[1].plot(l_T,d_Hm, 'o', label = " Dérivée de Hm (J.m/s) ")
     axs[2].plot(l_T,d_Hh, '+', label = " Dérivée de Hh (J.m/S) ")
     plt.legend()
 
-def moy_E_k(V,B):
-    E_k = E_kn(V,B)
+def moy_E_k(V,B, parametre):
+    E_k = E_kn(V,B, parametre)
     moy_E_k = np.mean(E_k, axis=0)
     return moy_E_k
 
-def show_E_k(V,B):
+def show_E_k(V,B, parametre):
+    k = parametre[3]
+
     plt.figure()
-    plt.plot(k,moy_E_k(V,B), 'x')
+    plt.plot(k,moy_E_k(V,B, parametre), 'x')
     plt.xscale('log')
     plt.yscale('log')
     plt.xlabel("Wavenumber")
     plt.ylabel("Spectral Energy (J.m/s)")
 
 
-def fit_simple(X,Y):
+def fit_simple(X,Y, parametre):
+    k = parametre[3]
     x = 10**4
     difference_array = np.absolute(X-x)
     index = difference_array.argmin()
