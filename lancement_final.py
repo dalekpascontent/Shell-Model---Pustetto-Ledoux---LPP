e431b5455bd966e64334ada39d7f97d9de993ede
Azuria-p <axel.pustetto@gmail.com>
2026-07-01 16:20:49 +0200

    Update lancement_final.py

--- lancement_final.py
+++ lancement_final.py
@@ -8,19 +8,17 @@ k0 = 0.01
 time_moy = 0.05 #temps de moyennage temporel (ici 5% de T_max)
 nb_sauvegarde = 2000000  # c'est le nombre selon lequel  on sauvegarde des points temporels (tous les 200 points j'en sauvegarde 1, car sinon overflow)
 lissage = True
-mur = 0.3 # % de l'energie initiale permettant d'omologuer une couche comme étant "active" (donc avec encore assez d'energie pour ne pas etre négligeable)
 ############### Paramètres de simulation #############
 
 regimes = {
-    "HD": {"N": 25, "T_max": 300, "i": 2, "CFL": 0.05, "Umoyen": 1, "Bmoyen": 0, "di": 0, "nu": 1e-13, "eta": 1e-13},
-    "EMHD": {"N": 25, "T_max": 500, "i": 5, "CFL": 0.4, "Umoyen": 0, "Bmoyen": 1, "di": 0.3, "nu": 1e-13, "eta": 1e-13},
+    "HD": {"N": 25, "T_max": 300, "i": 2, "CFL": 0.05, "mur": 0.05, "Umoyen": 1, "Bmoyen": 0, "di": 0, "nu": 1e-13, "eta": 1e-13},
 }
 
 """
 regimes = {
     "HD": {"labels": "HydroDynamique di = 0, $\\nu = 10^{-13}$", "Umoyen": 1, "Bmoyen": 0, "di": 0, "nu": 1e-13, "eta": 1e-13},
     "MHD": {"labels": "MHD di = 0", "Umoyen": 1, "Bmoyen": 1, "di": 0, "nu": 1e-13, "eta": 1e-13},
-    "EMHD": {"labels": "EMHD $di = 0.3, V= 0$, $\\eta = 10^{-13}$", "Umoyen": 0, "Bmoyen": 1, "di": 0.3, "nu": 1e-13, "eta": 1e-13},
+    "EMHD": {"N": 25, "T_max": 400, "i": 6, "CFL": 0.4, "Umoyen": 0, "Bmoyen": 1, "di": 0.3, "nu": 1e-13, "eta": 1e-13},
     "HMHD_s": {"labels": "HMHD di = 0.3, $\\eta = 1^{-13}$", "Umoyen": 1, "Bmoyen": 1, "di": 0.3, "nu": 1e-13, "eta": 1e-13},
     "HMHD_b": {"labels": "HMHD di = 1", "Umoyen": 1, "Bmoyen": 1, "di": 1, "nu": 1e-13, "eta": 1e-13}
 }
@@ -29,8 +27,9 @@ regimes = {
 simulations = []
 labels = []
 liste = []
-pentes_theoriques = {"HD": -5/3, "MHD": None, "EMHD": -7/3, "HMHD": -11/3}
+pentes_theoriques = {"HD": -5/3, "MHD": -5/3, "EMHD": -7/3, "HMHD_s": -11/3, "HMHD_b": -11/3}
 pentes = []
+mure = [] # mur =  % de l'energie initiale permettant d'omologuer une couche comme étant "active" (donc avec encore assez d'energie pour ne pas etre négligeable)
 
 ############### RUn des simulations ##############
 
@@ -43,6 +42,7 @@ for cas, para in regimes.items():
     
     V_nul = (cas == "EMHD") # V_nul est une condition donc un booleen
     pentes.append(pentes_theoriques[cas])
+    mure.append(para["mur"])
 
     U, B, l_T, liste_dt = ff.run_simu(parametre, nb_sauvegarde, V_nul)
     simulations.append((U, B, parametre, l_T))
@@ -51,7 +51,7 @@ for cas, para in regimes.items():
     print(f"Points et temps de simulation: {len(l_T)} , {l_T[-1]}")
 
 ff.show_Inv(simulations, labels)
-ff.show_spectre(simulations, labels, pentes, mur)
+ff.show_spectre(simulations, labels, pentes, mure)
 ff.show_dt(liste, labels)
 
 plt.show()
