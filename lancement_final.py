62422a2a545cdd768d45d901c537ae19c5253d36
Azuria-p <axel.pustetto@gmail.com>
2026-07-02 16:03:48 +0200

    Update lancement_final.py

--- lancement_final.py
+++ lancement_final.py
@@ -11,15 +11,15 @@ lissage = True
 ############### Paramètres de simulation #############
 
 regimes = {
-    "HD": {"N": 25, "T_max": 300, "i": 2, "CFL": 0.05, "mur": 0.05, "Umoyen": 1, "Bmoyen": 0, "di": 0, "nu": 1e-13, "eta": 1e-13},
+    "HMHD_b": {"N": 25, "T_max": 400, "i": 4, "CFL": 0.4, "mur": 0.05, "Umoyen": 1, "Bmoyen": 1, "di": 30, "nu": 1e-12, "eta": 1e-11},
 }
 
 """
 regimes = {
-    "HD": {"labels": "HydroDynamique di = 0, $\\nu = 10^{-13}$", "Umoyen": 1, "Bmoyen": 0, "di": 0, "nu": 1e-13, "eta": 1e-13},
+    "HD": {"N": 25, "T_max": 300, "i": 2, "CFL": 0.05, "mur": 0.05, "Umoyen": 1, "Bmoyen": 0, "di": 0, "nu": 1e-13, "eta": 1e-13},
     "MHD": {"labels": "MHD di = 0", "Umoyen": 1, "Bmoyen": 1, "di": 0, "nu": 1e-13, "eta": 1e-13},
-    "EMHD": {"N": 25, "T_max": 400, "i": 6, "CFL": 0.4, "Umoyen": 0, "Bmoyen": 1, "di": 0.3, "nu": 1e-13, "eta": 1e-13},
-    "HMHD_s": {"labels": "HMHD di = 0.3, $\\eta = 1^{-13}$", "Umoyen": 1, "Bmoyen": 1, "di": 0.3, "nu": 1e-13, "eta": 1e-13},
+    "EMHD": {"N": 25, "T_max": 400, "i": 6, "CFL": 0.4, "mur": 0.05, "Umoyen": 0, "Bmoyen": 1, "di": 0.3, "nu": 1e-13, "eta": 1e-13},
+    "HMHD_s": {"N": 25, "T_max": 400, "i": 4, "CFL": 0.4, "mur": 0.05, "Umoyen": 1, "Bmoyen": 1, "di": 0.3, "nu": 1e-12, "eta": 1e-12},
     "HMHD_b": {"labels": "HMHD di = 1", "Umoyen": 1, "Bmoyen": 1, "di": 1, "nu": 1e-13, "eta": 1e-13}
 }
 """
@@ -27,14 +27,14 @@ regimes = {
 simulations = []
 labels = []
 liste = []
-pentes_theoriques = {"HD": -5/3, "MHD": -5/3, "EMHD": -7/3, "HMHD_s": -11/3, "HMHD_b": -11/3}
+pentes_theoriques = {"HD": -5/3, "MHD": -5/3, "EMHD": -7/3, "HMHD_s": -11/3, "HMHD_b": -11/3, "HMHD_s": -7/3, "HMHD_b": -11/3}
 pentes = []
 mure = [] # mur =  % de l'energie initiale permettant d'omologuer une couche comme étant "active" (donc avec encore assez d'energie pour ne pas etre négligeable)
 
 ############### RUn des simulations ##############
 
 for cas, para in regimes.items():
-    parametre = [para["N"], para["T_max"], time_moy, para["Umoyen"], para["Bmoyen"], para["di"], para["nu"], para["eta"], para["i"], para["CFL"]]
+    parametre = [para["N"], para["T_max"], time_moy, para["Umoyen"], para["Bmoyen"], para["di"], para["nu"], para["eta"], para["i"], para["CFL"], f"{cas}"]
      #[nb de couches, temps max, k, % de moyennage, U départ, B départ, di, nu, eta, centre de gaussienne, coefficient CFL]
     k = np.array([k0*(2**i) for i in range(parametre[0])])
     parametre.insert(2, k)
@@ -51,7 +51,12 @@ for cas, para in regimes.items():
     print(f"Points et temps de simulation: {len(l_T)} , {l_T[-1]}")
 
 ff.show_Inv(simulations, labels)
-ff.show_spectre(simulations, labels, pentes, mure)
+package = ff.show_EVB(simulations, labels, pentes, mure)
 ff.show_dt(liste, labels)
+
+if package == 0:
+    None
+else:
+    ff.ratio_EB(package)
 
 plt.show()
